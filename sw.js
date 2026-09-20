fcc6fa441ade6eb59085b130cd3645dd53f5740b
inutake86 <inutake86@hotmail.com>
2026-09-21 00:15:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE='my-life-v038-onedrive-test';
+const CACHE='my-life-v0381-onedrive-test';
 const ASSETS=['./','./index.html','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith((async()=>{
+    try{return await fetch(e.request);}
+    catch(err){
+      const cached=await caches.match(e.request,{ignoreSearch:true});
+      if(cached) return cached;
+      return Response.error();
+    }
+  })());
+});
