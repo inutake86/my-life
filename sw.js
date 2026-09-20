3f6bc13674890b8a047e128e79f6ff0336db2008
inutake86 <inutake86@hotmail.com>
2026-09-21 03:10:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-life-v0.44.3-rating-master-graphfix';
+const CACHE='my-life-v0.45-rating-master-load';
 const ASSETS=['./','./index.html','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
